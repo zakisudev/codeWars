7715b75042b191332ad453a4fbcb3dc0697f9c3e
zakisudev@gmail.com <zakisu250@gmail.com>
2023-01-03 16:08:05 +0300

    palindrome chackfunction

---
+++ palindrome.js
@@ -0,0 +1,5 @@
+function isPalindrome(str) {
+  const cleanStr = str.toLowerCase().replace(/[^a-zA-Z0-9]/g, '');
+  const reversedStr = cleanStr.split('').reverse().join('');
+  return cleanStr === reversedStr;
+}
